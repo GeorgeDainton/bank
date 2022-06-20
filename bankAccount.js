fee0ba39fb6dae637fe618a5b03e1e66acaaf53b
George Dainton <gcdainton94@gmail.com>
2022-06-20 21:37:36 +0100

    refactored so deposit and withdraw are in one transact method

--- bankAccount.js
+++ bankAccount.js
@@ -1,52 +1,34 @@
 class BankAccount {
   constructor() {
     this.balance = 0
-    this.deposits = []
-    this.withdrawals = []
+    this.transactions = []
   }
 
-  deposit(amount) {
+  transact(amount) {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
     today = dd + '/' + mm + '/' + yyyy;
     
-    const depositRecord = {
+    const transactionRecord = {
       amount: amount,
       date: today,
       currentBalance : this.balance + amount
     };
     
-    this.deposits.push(depositRecord)
+    this.transactions.push(transactionRecord)
     this.balance += amount
-  }
-
-  withdraw(amount) {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
-    today = dd + '/' + mm + '/' + yyyy;
-   
-    const withdrawalRecord = {
-      amount: amount,
-      date: today,
-      currentBalance : this.balance - amount 
-      
-    };
-    
-    this.withdrawals.push(withdrawalRecord)
-    this.balance -= amount
   }
 
   printStatement() {
     
-    return this.deposits.map(deposit =>
-      (`credit ${deposit.date} ${deposit.amount} balance ${deposit.currentBalance}`)
+    return this.transactions.map(transaction =>
+      (`credit ${transaction.date} ${transaction.amount} balance ${transaction.currentBalance}`)
       )
 }
 }
+ 
       
 
 

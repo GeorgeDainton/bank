2cc51434d4d38b9926a092a463255288e81d4502
George Dainton <gcdainton94@gmail.com>
2022-06-20 16:17:48 +0100

    Refactored date format on deposit method

--- bankAccount.js
+++ bankAccount.js
@@ -5,9 +5,14 @@ class BankAccount {
   }
 
   deposit(amount) {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
     
     const depositRecord = {
-      date: new Date(),
+      date: today,
       amount: amount
     };
     

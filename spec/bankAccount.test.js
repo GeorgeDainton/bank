e8a19cf86d6f3b4fbbc0958ac6dc24124d190a3c
George Dainton <gcdainton94@gmail.com>
2022-06-21 17:03:58 +0100

    Reinstated deposit and withdraw methods

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -10,74 +10,74 @@ describe('Bank Account', () => {
 
   it('Adds deposit amounts to the balance', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(500)
+    bankAccount.deposit(500)
 
     expect(bankAccount.balance).toEqual(500)
   })
 
-  it('Adds record of deposit with a date and amount to transactions array', () => {
+  it('Adds record of deposit with a credit status, date and amount to transactions array', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(1000)
+    bankAccount.deposit(1000)
 
-    expect(bankAccount.transactions).toEqual([{ date: mockDate, amount: 1000, currentBalance: 1000 }])
+    expect(bankAccount.transactions).toEqual([{ credit: true, date: mockDate, amount: 1000, currentBalance: 1000 }])
   })
 
   it('Subtracts withdrawal amounts from the balance', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(5000)
-    bankAccount.transact(-500)
+    bankAccount.deposit(5000)
+    bankAccount.withdraw(500)
 
     expect(bankAccount.balance).toEqual(4500)
   })
 
   it('Adds record of withdrawal with a date and amount to transactions array', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(1000)
-    bankAccount.transact(-100)
+    bankAccount.deposit(1000)
+    bankAccount.withdraw(100)
 
-    expect(bankAccount.transactions).toEqual([{ date: mockDate, amount: 1000, currentBalance: 1000 }, { date: mockDate, amount: -100, currentBalance: 900 }])
+    expect(bankAccount.transactions).toEqual([{ credit: true, date: mockDate, amount: 1000, currentBalance: 1000 }, { credit: false, date: mockDate, amount: 100, currentBalance: 900 }])
   })
 
   it('Prints a statement that shows the balance', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(100)
+    bankAccount.deposit(100)
 
     expect(bankAccount.printStatement()).toContain['balance 100']
   })
 
   it('Prints a statement that shows deposit amounts', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(400)
+    bankAccount.deposit(400)
 
-    expect(bankAccount.printStatement()).toContain(`400`)
+    expect(bankAccount.printStatement()).toContain('400')
   })
 
   it('Prints a statement that shows withdrawal amounts', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(400)
-    bankAccount.transact(-300)
+    bankAccount.deposit(400)
+    bankAccount.withdraw(300)
 
-    expect(bankAccount.printStatement()).toContain(`-300`)
+    expect(bankAccount.printStatement()).toContain('300')
   })
 
   it('Prints a statement that shows transaction dates', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(400)
+    bankAccount.deposit(400)
 
     expect(bankAccount.printStatement()).toContain(`${mockDate}`)
   })
 
   it('Prints a statement with formatting', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(400)
-    bankAccount.transact(-300)
+    bankAccount.deposit(400)
+    bankAccount.withdraw(300)
 
-    expect(bankAccount.printStatement()).toContain(`date         || credit || debit || balance\n${mockDate} || 400 ||       || 400\n${mockDate} ||        || -300|| 100\n`)
+    expect(bankAccount.printStatement()).toContain(`date         || credit || debit || balance\n${mockDate} || 400 ||       || 400\n${mockDate} ||        || 300|| 100\n`)
   })
 
-  it('Does not allow withdrawal if accoutn has insufficient funds', () => {
+  it('Does not allow withdrawal if account has insufficient funds', () => {
     const bankAccount = new BankAccount()
-    bankAccount.transact(-1)
+    bankAccount.withdraw(1)
 
     expect(bankAccount.balance).toEqual(0)
   })
